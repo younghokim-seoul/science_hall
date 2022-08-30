c93b1d8c6f321fd6c4af97bdf43315c0aac88400
김영호 <ksemfn@gmail.com>
2022-08-30 23:30:05 +0900

    텍스트 스케일링 고정

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,12 @@ class App extends ConsumerWidget {
       splitScreenMode: false,
       builder: (context, child) {
         return MaterialApp.router(
+            builder: (context, child) {
+              return MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: child!,
+              );
+            },
           theme: theme.data,
           darkTheme: AppTheme.light().data,
           themeMode: themeMode,
